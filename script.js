fe58777097a08695d4357af026961685df351abe
yashjha2510 <yashjha2510@gmail.com>
2025-09-18 23:40:53 +0530

    build a basic ready to view site

---
+++ script.js
@@ -0,0 +1,6 @@
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+var typed = new Typed('#element', {
+      strings: ['Student &amp; a Web Developer', 'a Java Developer', 'SQL &amp; DBMS expert', '&amp; Aspiring Full Stack Developer.'],
+      typeSpeed: 50,
+    });
